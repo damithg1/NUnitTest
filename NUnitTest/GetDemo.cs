c78e63f0a1935a677bf487f12b3da3f1135c7e74
Damith Gunarathna <Damith.Gunarathna@acumatica.com>
2023-07-20 23:53:49 +0530

    Add project files.

---
+++ NUnitTest/GetDemo.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace YourNamespace
+{
+    public class GetDemo
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public Dictionary<string, object> Data { get; set; }
+    }
+
+    [TestFixture]
+    public class GetDemo2
+    {
+        private RestClient client;
+        private RestRequest request;
+        private List<GetDemo> devices;
+        RestResponse response = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            client = new RestClient("https://api.restful-api.dev");
+            request = new RestRequest("objects", Method.Get);
+
+            // Fetch and deserialize the devices data from the API
+            response = client.Execute(request);
+            devices = JsonConvert.DeserializeObject<List<GetDemo>>(response.Content);
+
+        }
+
+        [Test]
+        public void TestGetObjectsEndpoint()
+        {
+            try
+            {
+                response = client.Execute(request);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Exception occurred during request execution: " + ex.Message);
+            }
+
+            // Check if the response is successful
+            Assert.IsTrue(response.IsSuccessful, "Request was not successful.");
+
+            // Verify status code
+            Assert.That((int)response.StatusCode, Is.EqualTo(200), "Expected status code 200, but received " + (int)response.StatusCode);
+
+            // Verify content type
+            Assert.IsTrue(response.ContentType.StartsWith("application/json"), "Unexpected content type.");
+
+            // Verify response body is not empty
+            Assert.IsFalse(string.IsNullOrWhiteSpace(response.Content), "Response body is empty.");
+
+            // Verify response headers (optional)
+            Assert.IsTrue(response.Headers.Count > 0, "Response headers are missing.");
+
+            // Verify the total records count
+            int expectedRecordCount = 13;
+            Assert.AreEqual(expectedRecordCount, devices.Count);
+        }
+
+        [Test]
+        public void TestSearchDeviceById()
+        {
+            string deviceIdToSearch = "12";
+
+            // Find the device with the given ID
+            GetDemo foundDevice = devices.FirstOrDefault(d => d.Id == deviceIdToSearch);
+
+            // Verify status code
+            Assert.That((int)response.StatusCode, Is.EqualTo(200), "Expected status code 200, but received " + (int)response.StatusCode);
+
+            // Verify that the device with the given ID is found
+            Assert.IsNotNull(foundDevice, $"No device found with ID '{deviceIdToSearch}'.");
+
+            // Perform additional assertions or actions with the found device if needed
+            // For example, you can assert specific details for the device
+            Assert.IsTrue(foundDevice.Data.ContainsKey("Generation"), $"Generation not found for device {foundDevice.Id}.");
+            Assert.IsTrue(foundDevice.Data.ContainsKey("Price"), $"Price not found for device {foundDevice.Id}.");
+            Assert.IsTrue(foundDevice.Data.ContainsKey("Capacity"), $"Capacity not found for device {foundDevice.Id}.");
+
+            Assert.IsTrue(foundDevice.Name.Contains("Apple iPad Air"), $"Device with ID '{foundDevice.Id}' does not have 'Apple' in the name.");
+            //Assert.IsTrue(foundDevice.Data.Contains("4th"), $"Invalid screen size for device {foundDevice.Id}.");
+
+            // Perform validations on the device properties
+            Assert.That(foundDevice.Name, Is.EqualTo("Apple iPad Air"), $"Incorrect Name for device with ID {foundDevice.Id}.");
+            Assert.IsTrue(foundDevice.Data.ContainsKey("Generation"), $"Generation not found for device {foundDevice.Id}.");
+            Assert.That(foundDevice.Data["Generation"], Is.EqualTo("4th"), $"Incorrect Generation for device with ID {foundDevice.Id}.");
+            Assert.IsTrue(foundDevice.Data.ContainsKey("Price"), $"Price not found for device {foundDevice.Id}.");
+            Assert.That(foundDevice.Data["Price"], Is.EqualTo("419.99"), $"Incorrect Price for device with ID {foundDevice.Id}.");
+            Assert.IsTrue(foundDevice.Data.ContainsKey("Capacity"), $"Capacity not found for device {foundDevice.Id}.");
+            Assert.That(foundDevice.Data["Capacity"], Is.EqualTo("64 GB"), $"Incorrect Capacity for device with ID {foundDevice.Id}.");
+        }
+
+
+        [Test]
+        public void TestSearchDeviceByName()
+        {
+            response = client.Execute(request);
+          
+            //Assert.Fail("Exception occurred during request execution: " + ex.Message);
+          
+            string deviceNameToSearch = "Apple iPad Mini 5th Gen";
+
+            // Find the devices with the given name
+            List<GetDemo> foundDevice = devices.Where(d => d.Name == deviceNameToSearch).ToList();
+
+            // Verify status code
+            Assert.That((int)response.StatusCode, Is.EqualTo(200), "Expected status code 200, but received " + (int)response.StatusCode);
+
+            // Verify that the devices with the given name are found
+            Assert.IsTrue(foundDevice.Count > 0, $"No devices found with the name '{deviceNameToSearch}'.");
+
+            // Perform additional assertions or actions with the found devices if needed
+            foreach (GetDemo device in foundDevice)
+            {
+                // Verify Capacity, and Screen size fields
+                Assert.IsTrue(device.Data.ContainsKey("Capacity"), $"Capacity not found for device {device.Id}.");
+                Assert.IsTrue(device.Data.ContainsKey("Screen size"), $"Screen size not found for device {device.Id}.");
+            }
+        }
+
+       
+        [Test]
+        public void TestFilterRecordsByIdsUsingQueryParameter()
+        {
+            // Add query parameter to filter by IDs (3, 4, and 5)
+            request.AddQueryParameter("id", "3,4,10");
+
+            // Fetch and deserialize the devices data from the API
+            RestResponse response = client.Execute(request);
+
+            devices = JsonConvert.DeserializeObject<List<GetDemo>>(response.Content);
+            // IDs to filter
+            List<string> idsToFilter = new List<string> { "3", "4", "10" };
+
+            // Verify status code
+            Assert.That((int)response.StatusCode, Is.EqualTo(200), "Expected status code 200, but received " + (int)response.StatusCode);
+
+            // Assert that the expected number of records are present
+            Assert.AreEqual(idsToFilter.Count, devices.Count, $"Expected {idsToFilter.Count} records with IDs {string.Join(", ", idsToFilter)}, but found {devices.Count}.");
+
+            // Assert each filtered device with the specified IDs is present
+            foreach (string id in idsToFilter)
+            {
+                Assert.IsTrue(devices.Any(d => d.Id == id), $"Device with ID '{id}' not found.");
+            }
+
+            // Additional assertions on the filtered devices, if needed
+            foreach (GetDemo filteredDevice in devices)
+            {
+                Assert.IsTrue(filteredDevice.Name.Contains("Apple"), $"Device with ID '{filteredDevice.Id}' does not have 'Apple' in the name.");
+                //Assert.IsTrue(filteredDevice.Data.ContainsKey("Capacity"), $"Capacity not found for device {filteredDevice.Id}.");
+                //Assert.IsTrue(Convert.ToDouble(filteredDevice.Data["Screen size"]) > 0, $"Invalid screen size for device {filteredDevice.Id}.");
+            }
+        }
+
+    }
+}
+
---
+++ NUnitTest/PostDemo.cs
@@ -0,0 +1,89 @@
+using System;
+using NUnit.Framework;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace YourNamespace
+{
+    public class PostDeviceData
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public DeviceData2 Data { get; set; }
+    }
+
+    public class DeviceData2
+    {
+        public string Color { get; set; }
+        public string Capacity { get; set; }
+    }
+
+    [TestFixture]
+    public class RestSharpTest
+    {
+        private RestClient client;
+        private RestRequest request;
+
+        [SetUp]
+        public void Setup()
+        {
+            client = new RestClient("https://api.restful-api.dev/");
+
+            request = new RestRequest("objects", Method.Post);
+        }
+
+        [Test]
+        public void TestPostNewDevice()
+        {
+            // Create the new device data to be sent in the request
+            PostDeviceData newDevice = new PostDeviceData
+            {
+                Id = "123",
+                Name = "Google Pixel 6 Pro",
+                Data = new DeviceData2
+                {
+                    Color = "Cloudy White",
+                    Capacity = "128 GB"
+                }
+            };
+
+            // Serialize the PostDeviceData object to JSON and add it to the request body
+            string jsonBody = JsonConvert.SerializeObject(newDevice);
+
+            // Send POST request to create the new device
+            request = new RestRequest("objects", Method.Post);
+            request.AddJsonBody(jsonBody);
+            RestResponse response = client.Execute(request);
+
+            // Validate the status code
+            Assert.AreEqual(200, (int)response.StatusCode, $"Unexpected status code: {response.StatusCode}");
+
+            // Deserialize the response content into PostDeviceData object
+            PostDeviceData addedDevice = JsonConvert.DeserializeObject<PostDeviceData>(response.Content);
+
+            // Validate the response body structure
+            Assert.IsNotNull(addedDevice.Id, "The added device does not have an ID.");
+            Assert.IsNotNull(addedDevice.Name, "The added device does not have a Name.");
+            Assert.IsNotNull(addedDevice.Data, "The added device does not have Data.");
+
+            // Validate specific property values
+            Assert.AreEqual(newDevice.Name, addedDevice.Name, "Incorrect Name in the added device.");
+            Assert.AreEqual(newDevice.Data.Color, addedDevice.Data.Color, "Incorrect Color in the added device.");
+            Assert.AreEqual(newDevice.Data.Capacity, addedDevice.Data.Capacity, "Incorrect Capacity in the added device.");
+
+            // Cleanup: Send DELETE request to remove the added device
+            request = new RestRequest($"objects/{addedDevice.Id}", Method.Delete);
+            response = client.Execute(request);
+
+            // Validate the DELETE response status code (Assuming 204 No Content as the expected status code for successful deletion)
+            Assert.AreEqual(204, (int)response.StatusCode, $"Unexpected status code for DELETE request: {response.StatusCode}");
+
+            // Verify that the device is deleted by sending a GET request for the same ID
+            request = new RestRequest($"objects/{addedDevice.Id}", Method.Get);
+            response = client.Execute(request);
+
+            // Validate the GET response status code (Assuming 404 Not Found as the expected status code for a deleted resource)
+            Assert.AreEqual(404, (int)response.StatusCode, $"Unexpected status code for GET request after deletion: {response.StatusCode}");
+        }
+    }
+}
